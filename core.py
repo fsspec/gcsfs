e956bb86992751728bb604023511047833b9ce88
Martin Durant <martin.durant@utoronto.ca>
2016-12-19 16:54:46 -0500

    Initial commit

---
+++ core.py
@@ -0,0 +1,474 @@
+# -*- coding: utf-8 -*-
+"""
+Google Cloud Storage pythonic interface
+"""
+from __future__ import print_function, unicode_literals
+
+import json
+import logging
+import oauth2client.client   # version 1.5.2
+import os
+import pickle
+import re
+import requests
+import time
+import webbrowser
+
+from s3fs.utils import read_block
+
+logger = logging.getLogger(__name__)
+
+not_secret = {"client_id": "586241054156-is96mugvl2prnj0ib5gsg1l3q9m9jp7p.apps.googleusercontent.com",
+              "client_secret": "_F-W4r2HzuuoPvi6ROeaUB6o"}
+tfile = os.path.join(os.path.expanduser("~"), '.gcs_tokens')
+
+
+def split_path(path):
+    """
+    Normalise S3 path string into bucket and key.
+
+    Parameters
+    ----------
+    path : string
+        Input path, like `s3://mybucket/path/to/file`
+
+    Examples
+    --------
+    >>> split_path("s3://mybucket/path/to/file")
+    ['mybucket', 'path/to/file']
+    """
+    if path.startswith('gcs://'):
+        path = path[6:]
+    if '/' not in path:
+        return path, ""
+    else:
+        return path.split('/', 1)
+
+
+class GCSFileSystem(object):
+    """
+    Connect to Google Cloud Storage.
+
+    We maintain a cache of refresh tokens in the file ~/.gcs_tokens, which
+    means that while you'll have to authorize new connections the first
+    time you use them (in the browser), you should not have to do that again
+    unless Google itself purges tokens at some point.
+
+    Parameters
+    ----------
+    name : string
+        Identifier for this connection, typically your google user,
+        or just blank for default
+    project : string
+        GCS users are usually within projects. This is required in order
+        to list all the buckets you have access to.
+    access : one of {'read_only', 'read_write', 'full_control'}
+        Full control implies read/write as well as modifying metadata,
+        e.g., access control.
+    """
+    scopes = {'read_only', 'read_write', 'full_control'}
+    retries = 10
+    base = "https://www.googleapis.com/storage/v1/"
+
+    def __init__(self, name='', project='', access='full_control'):
+        if access not in self.scopes:
+            raise ValueError('acess must be one of %s', self.scopes)
+        self.name = name
+        self.project = project
+        self.access = access
+        self.dirs = {}
+        self.connect()
+
+    @staticmethod
+    def load_tokens():
+        try:
+            with open(tfile, 'rb') as f:
+                tokens = pickle.load(f)
+        except Exception:
+            tokens = {}
+        GCSFileSystem.tokens = tokens
+
+    def connect(self, refresh=False):
+        name, access = self.name, self.access
+        if (name, access) in self.tokens:
+            data2 = self.tokens[(name, access)]
+        else:
+            scope = "https://www.googleapis.com/auth/devstorage." + access
+            r = requests.post('https://accounts.google.com/o/oauth2/device/code',
+                              params={'client_id':not_secret['client_id'],
+                                      'scope':scope})
+            if r.status_code != 200:
+                raise RuntimeError
+            data = json.loads(r.content.decode())
+            print('Enter the following code when prompted in the browser:')
+            print(data['user_code'])
+            webbrowser.open(data['verification_url'])
+            for i in range(self.retries):
+                time.sleep(data['interval'])
+                r = requests.post("https://www.googleapis.com/oauth2/v4/token",
+                                  params={'client_id': not_secret['client_id'],
+                            'client_secret': not_secret['client_secret'],
+                            'code': data['device_code'],
+                            'grant_type': "http://oauth.net/grant_type/device/1.0"})
+                data2 = json.loads(r.content.decode())
+                if 'error' in data2:
+                    if i == self.retries - 1:
+                        raise RuntimeError
+                    continue
+                break
+            data2['timestamp'] = time.time()
+        if refresh or time.time() - data2['timestamp'] > data2['expires_in'] - 100:
+            r = requests.post("https://www.googleapis.com/oauth2/v4/token",
+                    params={'client_id': not_secret['client_id'],
+                            'client_secret': not_secret['client_secret'],
+                            'refresh_token': data2['refresh_token'],
+                            'grant_type': "refresh_token"})
+            if r.status_code != 200:
+                raise RuntimeError
+            data2['timestamp'] = time.time()
+            data2['access_token'] = json.loads(r.content.decode())['access_token']
+        data = data2
+
+        self.tokens[(name, access)] = data
+        self.head = {'Authorization': 'Bearer ' + data['access_token']}
+        try:
+            with open(tfile, 'wb') as f:
+                pickle.dump(self.tokens, f)
+        except Exception:
+            pass
+
+    def _list_buckets(self):
+        if '' not in self.dirs:
+            r = requests.get(self.base + 'b/',
+                             headers=self.head, params={'project': self.project})
+            if r.status_code != 200:
+                raise RuntimeError
+            dirs = json.loads(r.content.decode())['items']
+            self.dirs[''] = dirs
+        return self.dirs['']
+
+    def _list_bucket(self, bucket):
+        if bucket not in self.dirs:
+            r = requests.get(self.base + 'b/%s/o/' % bucket,
+                             headers=self.head)
+            if r.status_code != 200:
+                raise RuntimeError
+            dirs = json.loads(r.content.decode())['items']
+            for f in dirs:
+                f['name'] = '%s/%s' % (bucket, f['name'])
+                f['size'] = int(f.get('size'), 0)
+            self.dirs[bucket] = dirs
+        return self.dirs[bucket]
+
+    def invalidate_cache(self, bucket):
+        if bucket == '/':
+            bucket = ''
+        self.dirs.pop(bucket, None)
+
+    def ls(self, path, detail=False):
+        if path in ['', '/']:
+            files = self._list_buckets()
+        else:
+            bucket, prefix = split_path(path)
+            files = self._list_bucket(bucket)
+            files = [f for f in files if f['name'].startswith(path)]
+        if detail:
+            return files
+        else:
+            return [f['name'] for f in files]
+
+    def walk(self, path):
+        return self.ls(path)
+
+    def glob(self, path):
+        """
+        Find files by glob-matching.
+
+        Note that the bucket part of the path must not contain a "*"
+        """
+        path0 = path
+        path = path.rstrip('/')
+        bucket, key = split_path(path)
+        if "*" in bucket:
+            raise ValueError('Bucket cannot contain a "*"')
+        if '*' not in path:
+            path = path.rstrip('/') + '/*'
+        if '/' in path[:path.index('*')]:
+            ind = path[:path.index('*')].rindex('/')
+            root = path[:ind + 1]
+        else:
+            root = '/'
+        allfiles = self.walk(root)
+        pattern = re.compile("^" + path.replace('//', '/')
+                             .rstrip('/')
+                             .replace('*', '[^/]*')
+                             .replace('?', '.') + "$")
+        out = [f for f in allfiles if re.match(pattern,
+               f.replace('//', '/').rstrip('/'))]
+        if not out:
+            out = self.ls(path0)
+        return out
+
+    def info(self, path):
+        # also have direct info call available
+        files = self.ls(path, True)
+        return [f for f in files if f['name']==path][0]
+
+    def cat(self, path):
+        """ Simple one-shot get of file data """
+        details = self.info(path)
+        return _fetch_range(self.head, details)
+
+    def open(self, path, mode='rb', block_size=5 * 2 ** 20):
+        return GCSFile(self, path, mode, block_size)
+
+    def read_block(self, fn, offset, length, delimiter=None):
+        """ Read a block of bytes from a GCS file
+
+        Starting at ``offset`` of the file, read ``length`` bytes.  If
+        ``delimiter`` is set then we ensure that the read starts and stops at
+        delimiter boundaries that follow the locations ``offset`` and ``offset
+        + length``.  If ``offset`` is zero then we start at zero.  The
+        bytestring returned WILL include the end delimiter string.
+
+        If offset+length is beyond the eof, reads to eof.
+
+        Parameters
+        ----------
+        fn: string
+            Path to filename on S3
+        offset: int
+            Byte offset to start read
+        length: int
+            Number of bytes to read
+        delimiter: bytes (optional)
+            Ensure reading starts and stops at delimiter bytestring
+
+        Examples
+        --------
+        >>> gcs.read_block('data/file.csv', 0, 13)  # doctest: +SKIP
+        b'Alice, 100\\nBo'
+        >>> gcs.read_block('data/file.csv', 0, 13, delimiter=b'\\n')  # doctest: +SKIP
+        b'Alice, 100\\nBob, 200\\n'
+
+        Use ``length=None`` to read to the end of the file.
+        >>> gcs.read_block('data/file.csv', 0, None, delimiter=b'\\n')  # doctest: +SKIP
+        b'Alice, 100\\nBob, 200\\nCharlie, 300'
+
+        See Also
+        --------
+        distributed.utils.read_block
+        """
+        with self.open(fn, 'rb') as f:
+            size = f.size
+            if length is None:
+                length = size
+            if offset + length > size:
+                length = size - offset
+            bytes = read_block(f, offset, length, delimiter)
+        return bytes
+
+GCSFileSystem.load_tokens()
+
+
+class GCSFile:
+
+    def __init__(self, gcsfs, path, mode='rb', block_size=5 * 2 ** 20):
+        bucket, key = split_path(path)
+        self.gcsfs = gcsfs
+        self.bucket = bucket
+        self.key = key
+        if mode not in {'rb'}:
+            raise ValueError('File mode not supported')
+        self.mode = mode
+        self.blocksize = block_size
+        self.cache = b""
+        self.loc = 0
+        self.start = None
+        self.end = None
+        self.closed = False
+        self.trim = True
+        self.details = gcsfs.info(path)
+        self.size = self.details['size']
+
+    def info(self):
+        """ File information about this path """
+        return self.details
+
+    def tell(self):
+        """ Current file location """
+        return self.loc
+
+    def seek(self, loc, whence=0):
+        """ Set current file location
+
+        Parameters
+        ----------
+        loc : int
+            byte location
+        whence : {0, 1, 2}
+            from start of file, current location or end of file, resp.
+        """
+        if not self.mode == 'rb':
+            raise ValueError('Seek only available in read mode')
+        if whence == 0:
+            nloc = loc
+        elif whence == 1:
+            nloc = self.loc + loc
+        elif whence == 2:
+            nloc = self.size + loc
+        else:
+            raise ValueError(
+                "invalid whence (%s, should be 0, 1 or 2)" % whence)
+        if nloc < 0:
+            raise ValueError('Seek before start of file')
+        self.loc = nloc
+        return self.loc
+
+    def readline(self, length=-1):
+        """
+        Read and return a line from the stream.
+
+        If length is specified, at most size bytes will be read.
+        """
+        self._fetch(self.loc, self.loc + 1)
+        while True:
+            found = self.cache[self.loc - self.start:].find(b'\n') + 1
+            if length > 0 and found > length:
+                return self.read(length)
+            if found:
+                return self.read(found)
+            if self.end > self.size:
+                return self.read(length)
+            self._fetch(self.start, self.end + self.blocksize)
+
+    def __next__(self):
+        data = self.readline()
+        if data:
+            return data
+        else:
+            raise StopIteration
+
+    next = __next__
+
+    def __iter__(self):
+        return self
+
+    def readlines(self):
+        """ Return all lines in a file as a list """
+        return list(self)
+
+    def _fetch(self, start, end):
+        if self.start is None and self.end is None:
+            # First read
+            self.start = start
+            self.end = end + self.blocksize
+            self.cache = _fetch_range(self.gcsfs.head, self.details,
+                                      start, self.end)
+        if start < self.start:
+            new = _fetch_range(self.gcsfs.head, self.details,
+                               start, self.start)
+            self.start = start
+            self.cache = new + self.cache
+        if end > self.end:
+            if self.end > self.size:
+                return
+            new = _fetch_range(self.gcsfs.head, self.details,
+                               self.end, end + self.blocksize)
+            self.end = end + self.blocksize
+            self.cache = self.cache + new
+
+    def read(self, length=-1):
+        """
+        Return data from cache, or fetch pieces as necessary
+
+        Parameters
+        ----------
+        length : int (-1)
+            Number of bytes to read; if <0, all remaining bytes.
+        """
+        if self.mode != 'rb':
+            raise ValueError('File not in read mode')
+        if length < 0:
+            length = self.size
+        if self.closed:
+            raise ValueError('I/O operation on closed file.')
+        self._fetch(self.loc, self.loc + length)
+        out = self.cache[self.loc - self.start:
+                         self.loc - self.start + length]
+        self.loc += len(out)
+        if self.trim:
+            num = (self.loc - self.start) // self.blocksize - 1
+            if num > 0:
+                self.start += self.blocksize * num
+                self.cache = self.cache[self.blocksize * num:]
+        return out
+
+    def close(self):
+        """ Close file """
+        if self.closed:
+            return
+        self.cache = None
+        self.closed = True
+
+    def readable(self):
+        """Return whether the GCSFile was opened for reading"""
+        return self.mode == 'rb'
+
+    def seekable(self):
+        """Return whether the GCSFile is seekable (only in read mode)"""
+        return self.readable()
+
+    def writable(self):
+        """Return whether the GCSFile was opened for writing"""
+        return self.mode in {'wb', 'ab'}
+
+    def __del__(self):
+        self.close()
+
+    def __str__(self):
+        return "<GCSFile %s/%s>" % (self.bucket, self.key)
+
+    __repr__ = __str__
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+
+def _fetch_range(head, obj_dict, start=None, end=None):
+    """ Get data from GCS
+
+    head : dict
+        Contains authorization header
+    obj_dict : an entry from ls() or info()
+    start, end : None or integers
+        if not both None, fetch only given range
+    """
+    logger.debug("Fetch: %s/%s, %s-%s", obj_dict['name'], start, end)
+    if start is not None or end is not None:
+        start = start or 0
+        end = end or -0
+        head = head.copy()
+        head['Range'] = 'bytes=%i-%i' % (start, end - 1)
+    back = requests.get(obj_dict['mediaLink'], headers=head)
+    data = back.content
+    return data
+
+
+def put_object(credentials, bucket, name, data):
+    """ Simple put, up to 5MB of data
+
+    credentials : from auth()
+    bucket : string
+    name : object name
+    data : binary
+    """
+    out = requests.post('https://www.googleapis.com/upload/storage/v1/b/%s/o?uploadType=media&name=%s' %(
+                            bucket, name),
+                        headers={'Authorization': 'Bearer '+credentials.access_token,
+                                 'Content-Type':'application/octet-stream',
+                                 'Content-Length':len(data)}, data=data)
+    assert out.status_code == 200
