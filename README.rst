fd23d285efa230151eac7269941bb55098be1c37
Martin Durant <martin.durant@utoronto.ca>
2017-03-14 17:38:37 -0400

    Update doc config

--- README.rst
+++ README.rst
@@ -2,3 +2,8 @@ gcsfs
 =====
 
 Pythonic file-system for Google Cloud Storage
+
+
+For documentation, go to readthedocs_.
+
+.. _readthedocs: http://gcsfs.readthedocs.io/en/latest/
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# S3Fs documentation build configuration file, created by
+# GCSFs documentation build configuration file, created by
 # sphinx-quickstart on Mon Mar 21 15:20:01 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -53,7 +53,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'S3Fs'
+project = 'GCSFs'
 copyright = '2016, Continuum Analytics'
 author = 'Continuum Analytics'
 
@@ -62,8 +62,8 @@ author = 'Continuum Analytics'
 # built documents.
 #
 # The short X.Y version.
-import s3fs
-version = s3fs.__version__
+import gcsfs
+version = gcsfs.__version__
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -213,7 +213,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'S3Fsdoc'
+htmlhelp_basename = 'GCSFSdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -235,7 +235,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'S3Fs.tex', 'S3Fs Documentation',
+    (master_doc, 'GCSFs.tex', 'GCSFs Documentation',
      'Continuum Analytics', 'manual'),
 ]
 
@@ -265,7 +265,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 's3fs', 'S3Fs Documentation',
+    (master_doc, 'gcsfs', 'GCSFs Documentation',
      [author], 1)
 ]
 
@@ -279,8 +279,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'S3Fs', 'S3Fs Documentation',
-     author, 'S3Fs', 'One line description of project.',
+    (master_doc, 'GCSFs', 'GCSFs Documentation',
+     author, 'GCSFs', 'One line description of project.',
      'Miscellaneous'),
 ]
 
