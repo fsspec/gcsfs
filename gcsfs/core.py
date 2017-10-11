ef42a5360ca577ddacac312a1e44ad7520dba2c1
Martin Durant <martin.durant@utoronto.ca>
2017-10-11 12:22:38 -0400

    Add simple info without ls

--- gcsfs/core.py
+++ gcsfs/core.py
@@ -137,7 +137,7 @@ class GCSFileSystem(object):
       utility; this is either a python dictionary, or the name of a file
       containing the JSON returned by logging in with the gcloud CLI tool. On
       a posix system this may be at
-      ``~/.config/gcloud/application_default_credentials.json``
+      ``c``
 
     We maintain a cache of refresh tokens in the file ~/.gcs_tokens, so for any
     pair of (project, access), you will not need to log in once your credentials
@@ -514,13 +514,20 @@ class GCSFileSystem(object):
             return False
 
     def info(self, path):
-        path = '/'.join(split_path(path))
-        files = self.ls(path, True)
-        out = [f for f in files if f['name'] == path]
-        if out:
-            return out[0]
+        bucket, key = split_path(path)
+        if bucket not in self.dirs:
+            d = self._call('get', 'b/{}/o/{}', bucket, key)
+            d['name'] = '%s/%s' % (bucket, d['name'])
+            d['size'] = int(d.get('size'), 0)
+            return d
         else:
-            raise FileNotFoundError(path)
+            path = '/'.join(split_path(path))
+            files = self.ls(path, True)
+            out = [f for f in files if f['name'] == path]
+            if out:
+                return out[0]
+            else:
+                raise FileNotFoundError(path)
 
     def url(self, path):
         return self.info(path)['mediaLink']
