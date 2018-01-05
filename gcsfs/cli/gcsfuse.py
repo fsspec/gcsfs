994eae069fb52a04131a1cf6cf071560f9ec37f3
Matthew Rocklin <mrocklin@gmail.com>
2018-01-05 09:58:24 -0600

    small edits

--- gcsfs/cli/gcsfuse.py
+++ gcsfs/cli/gcsfuse.py
@@ -1,5 +1,7 @@
-from gcsfs.gcsfuse import FUSE, GCSFS
 import click
+from fuse import FUSE
+
+from gcsfs.gcsfuse import GCSFS
 
 
 @click.command()
@@ -7,13 +9,14 @@ import click
 @click.argument('mount_point', type=str, required=True)
 @click.option('--token', type=str, required=False, default=None,
               help="Token to use for authentication")
-@click.option('--project', type=str, required=False, default='',
-              help="Billing project")
+@click.option('--project-id', type=str, required=False, default='',
+              help="Billing Project ID")
 @click.option('--foreground/--background', default=True,
               help="Run in the foreground or as a background process")
-def main(bucket, mount_point, token, project, foreground):
+def main(bucket, mount_point, token, project_id, foreground):
+    """ Mount a Google Cloud Storage (GCS) bucket to a local directory """
     print("Mounting bucket %s to directory %s" % (bucket, mount_point))
-    FUSE(GCSFS(bucket, token=token, project=project),
+    FUSE(GCSFS(bucket, token=token, project=project_id),
          mount_point, nothreads=True, foreground=foreground)
 
 
