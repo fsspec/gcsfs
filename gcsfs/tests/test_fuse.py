8e5c1dd7a2ae56124116a772e0e4693a43a993d7
Martin Durant <martin.durant@utoronto.ca>
2018-01-11 12:20:07 -0500

    py2 thread

--- gcsfs/tests/test_fuse.py
+++ gcsfs/tests/test_fuse.py
@@ -3,6 +3,7 @@ import pytest
 fuse = pytest.importorskip('fuse')
 from gcsfs.gcsfuse import GCSFS
 import tempfile
+import sys
 from gcsfs.tests.settings import (TEST_BUCKET, TEST_PROJECT, RECORD_MODE,
                                   GOOGLE_TOKEN, FAKE_GOOGLE_TOKEN, DEBUG)
 from gcsfs.tests.utils import gcs_maker, token_restore, my_vcr
@@ -11,15 +12,26 @@ import threading
 import time
 
 
+PY2 = sys.version_info.major < 3
+
+
 @my_vcr.use_cassette(match=['all'])
 def test_fuse(token_restore):
     mountpath = tempfile.mkdtemp()
     with gcs_maker() as gcs:
         gcs.touch(TEST_BUCKET + '/hello')
-        th = threading.Thread(target=lambda:
+        if PY2:
+            th = threading.Thread(target=lambda:
             fuse.FUSE(
-                GCSFS(TEST_BUCKET, gcs=gcs), mountpath, nothreads=False, foreground=True),
-                      daemon=True)
+                GCSFS(TEST_BUCKET, gcs=gcs), mountpath, nothreads=False,
+                foreground=True))
+            th.daemon = True
+        else:
+            th = threading.Thread(target=lambda:
+                fuse.FUSE(
+                    GCSFS(TEST_BUCKET, gcs=gcs), mountpath, nothreads=False,
+                    foreground=True),
+                          daemon=True)
         th.start()
         time.sleep(2)
         files = os.listdir(mountpath)
