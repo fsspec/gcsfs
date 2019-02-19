4aeb1acf8924dba7e1817c2f0cea76aea4003c77
Yannick Einsweiler <ye@hintsight.net>
2019-02-19 17:32:33 -0500

    debug

--- gcsfs/core.py
+++ gcsfs/core.py
@@ -480,6 +480,7 @@ class GCSFileSystem(object):
                 validate_response(r, path)
                 break
             except (HtmlError, RequestException, RateLimitException, GoogleAuthError) as e:
+                print(e)
                 if retry == self.retries - 1:
                     logger.exception("_call out of retries on exception: %s", e)
                     raise e
