5feb858692655ba48a8f45143151ee12714ecb7e
Yannick Einsweiler <ye@hintsight.net>
2019-02-20 09:51:01 -0500

    429 is retriable

--- gcsfs/core.py
+++ gcsfs/core.py
@@ -480,8 +480,6 @@ class GCSFileSystem(object):
                 validate_response(r, path)
                 break
             except (HtmlError, RequestException, RateLimitException, GoogleAuthError) as e:
-                print(r.content)
-                print(r.json())
                 if retry == self.retries - 1:
                     logger.exception("_call out of retries on exception: %s", e)
                     raise e
