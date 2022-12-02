6bce670fb53c108861de016bb43763b2e06dd199
Brian Larsen <B_R_L@hotmail.com>
2022-12-02 16:06:27 +0000

    Set test timeout, debug fuse

--- gcsfs/tests/test_fuse.py
+++ gcsfs/tests/test_fuse.py
@@ -6,17 +6,20 @@ import time
 from functools import partial
 
 import pytest
-from fsspec.fuse import run
 
 from gcsfs.tests.settings import TEST_BUCKET
 
 
 @pytest.mark.timeout(180)
 @pytest.fixture
-def fuse():
+def fsspec_fuse_run():
+    """Fixture catches other errors on fuse import."""
     try:
-        _fuse = pytest.importorskip("fuse")
-        return _fuse
+        _fuse = pytest.importorskip("fuse")  # noqa
+
+        from fsspec.fuse import run as _fsspec_fuse_run
+
+        return _fsspec_fuse_run
     except Exception as error:
         logging.debug("Error importing fuse: %s", error)
         pytest.skip("Error importing fuse.")
@@ -24,18 +27,19 @@ def fuse():
 
 @pytest.mark.xfail(reason="Failing test not previously tested.")
 @pytest.mark.timeout(180)
-def test_fuse(gcs, fuse):
+def test_fuse(gcs, fsspec_fuse_run):
     mountpath = tempfile.mkdtemp()
-    _run = partial(run, gcs, TEST_BUCKET + "/", mountpath)
+    _run = partial(fsspec_fuse_run, gcs, TEST_BUCKET + "/", mountpath)
     th = threading.Thread(target=_run)
     th.daemon = True
     th.start()
 
     time.sleep(5)
     timeout = 20
-    while timeout > 0:
+    n = 40
+    for i in range(n):
+        logging.debug(f"Attempt # {i+1}/{n} to create lock file.")
         try:
-            logging.debug("Trying to create lock file.")
             open(os.path.join(mountpath, "lock"), "w").close()
             os.remove(os.path.join(mountpath, "lock"))
             break
@@ -44,6 +48,8 @@ def test_fuse(gcs, fuse):
             time.sleep(0.5)
         timeout -= 0.5
         assert timeout > 0
+    else:
+        raise AssertionError(f"Attempted lock file failed after {n} attempts.")
 
     with open(os.path.join(mountpath, "hello"), "w") as f:
         # NB this is in TEXT mode
