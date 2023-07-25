4541e4016b597fb1369ce8cec3db2c31012efabc
Altay Sansal <tasansal@users.noreply.github.com>
2023-07-25 14:30:40 -0500

    Add pickling support to `HttpError` class (#571)

--- gcsfs/tests/test_retry.py
+++ gcsfs/tests/test_retry.py
@@ -1,4 +1,7 @@
+import multiprocessing
 import os
+import pickle
+from concurrent.futures import ProcessPoolExecutor
 
 import pytest
 import requests
@@ -37,6 +40,37 @@ def test_retriable_exception():
 
     e = ProxyError()
     assert is_retriable(e)
+
+
+def test_pickle_serialization():
+    expected = HttpError({"message": "", "code": 400})
+
+    # Serialize/Deserialize
+    serialized = pickle.dumps(expected)
+    actual = pickle.loads(serialized)
+
+    is_same_type = type(expected) is type(actual)
+    is_same_args = expected.args == actual.args
+
+    assert is_same_type and is_same_args
+
+
+def conditional_exception(process_id):
+    # Raise only on second process (id=1)
+    if process_id == 1:
+        raise HttpError({"message": "", "code": 400})
+
+
+def test_multiprocessing_error_handling():
+    # Ensure spawn context to avoid forking issues
+    ctx = multiprocessing.get_context("spawn")
+
+    # Run on two processes
+    with ProcessPoolExecutor(2, mp_context=ctx) as p:
+        results = p.map(conditional_exception, range(2))
+
+    with pytest.raises(HttpError):
+        _ = [result for result in results]
 
 
 def test_validate_response():
