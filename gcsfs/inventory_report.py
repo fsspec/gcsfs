7954345c88c1de9873cc0ce0cc806f012ba4f081
Hans Easton <hanseaston@google.com>
2023-08-07 16:26:46 -0700

    Initialize barebone inventory report class structure and related methods

---
+++ gcsfs/inventory_report.py
@@ -0,0 +1,307 @@
+class InventoryReport:
+    """
+    A utility class for fetching and processing inventory reports from GCS.
+
+    The 'InventoryReport' class provides logic to support logic to fetch
+    inventory reports, and process their content to obtain a final snapshot
+    of objects in the latest inventory reports.
+
+    High-Level Functionality:
+    ------------------------
+    1. Fetching Inventory Reports:
+       - The class offers methods to fetch inventory report configurations and
+         metadata from GCS.
+       - It validates the inventory report information provided by the user.
+       - Inventory report configurations include options for parsing CSV format
+         and specifying the bucket and destination path.
+
+    2. Parsing and Processing Inventory Report Content:
+       - The class processes the raw content of inventory reports to extract
+         object details such as name, size, etc.
+       - It supports listing objects using a snapshot option or filtering
+         based on a user-defined prefix.
+       - The class handles CSV parsing, removes header (if specified), and
+         fetches required object metadata.
+
+    3. Constructing the Final Snapshot:
+       - If the user wishes to use the snapshot to do listing directly, the
+         snapshot will contain the relevant object details and subdirectory
+         prefixes, filtered by the prefix.
+
+       - If the user wishes to use the snapshot as a starting point for async
+         listing, the snapshot will only contain a list of object names,
+         filtered by the prefix.
+
+    Note:
+    -----
+    - The class should only be internally used in the 'GCSFileSystem' as an
+      optional configuration during listing.
+
+    Example Usage:
+    --------------
+    # Should already be instanted in 'core.py'
+    gcs_file_system = GCSFileSystem(...)
+
+    # User defines inventory report information
+    inventory_report_info = {
+        "use_snapshot_listing": True,
+        "location": "us-east1",
+        "id": "inventory_report_id"
+    }
+
+    # User defines a prefix for filtering objects
+    prefix = "prefix/"
+
+    # Fetch the snapshot based on inventory reports
+    items, prefixes = await InventoryReport.fetch_snapshot(
+    gcs_file_system, inventory_report_info, prefix)
+    """
+
+    # HTTP endpoint of the Storage Insights Service.
+    BASE_URL = "https://storageinsights.googleapis.com/v1"
+
+    @classmethod
+    async def fetch_snapshot(cls, gcs_file_system, inventory_report_info, prefix):
+        """
+        Main entry point of the 'InventoryReport' class.
+        Fetches the latest snapshot of objects based on inventory report configuration.
+
+        Parameters:
+            gcs_file_system (GCSFileSystem): An instance of the 'GCSFileSystem'
+            class (see 'core.py').
+            inventory_report_info (dict): A client-configured dictionary
+            containing inventory report information.
+            prefix (str): Listing prefix specified by the client. 
+
+        Returns:
+            tuple: A tuple containing two lists: the 'items' list representing
+            object details for the snapshot, and the 'prefixes' list containing
+            subdirectory prefixes.
+
+            Note: when 'use_snapshot_listing' in 'inventory_report_info' is set
+            to False, the 'prefixes' list will be empty, and the 'items' list
+            will contain only the object names. 
+        """
+        pass
+
+    def _validate_inventory_report_info(inventory_report_info):
+        """
+        Validates the inventory report information dictionary that user
+        passes in.
+
+        Parameters:
+            inventory_report_info (dict): A dictionary containing the inventory
+            report information with the following keys:
+                - "use_snapshot_listing" (bool): A flag indicating whether
+                  to use snapshot listing in the inventory report.
+                - "location" (str): The location of the inventory report in GCS.
+                - "id" (str): The ID of the inventory report in GCS.
+
+        Raises:
+            ValueError: If any required key (use_snapshot_listing, location, id)
+            is missing from the inventory_report_info dictionary.
+        """
+        pass
+
+    async def _fetch_raw_inventory_report_config(gcs_file_system, location, id):
+        """
+        Fetches the raw inventory report configuration from GCS based on the
+        specified location and ID.
+
+        Parameters:
+            gcs_file_system (GCSFileSystem): An instance of the 'GCSFileSystem'
+            class (see 'core.py').
+            location (str): The location of the inventory report in GCS.
+            id (str): The ID of the inventory report in GCS.
+
+        Returns:
+            dict: A dictionary containing the raw inventory report
+            configuration retrieved from GCS.
+
+        Raises:
+            Exception: If there is an error while fetching the inventory
+            report configuration.
+        """
+        pass
+
+    def _parse_raw_inventory_report_config(
+            raw_inventory_report_config, use_snapshot_listing):
+        """
+        Parses the raw inventory report configuration and validates its properties.
+
+        Parameters:
+            raw_inventory_report_config (dict): A dictionary containing the raw
+            inventory report configuration retrieved from GCS.
+            use_snapshot_listing (bool): A flag indicating whether to use snapshot
+            listing in the inventory report.
+
+        Returns:
+            InventoryReportConfig: An instance of the InventoryReportConfig
+            class representing the parsed inventory report configuration.
+
+        Raises:
+            ValueError: If the current date is outside the start and
+            end range specified in the inventory report config.
+            ValueError: If the "name" field is not present in the metadata
+            fields of the report config.
+            ValueError: If "size" field is not present in the metadata
+            fields and use_snapshot_listing is True.
+        """
+        pass
+
+    async def _fetch_inventory_report_metadata(
+            gcs_file_system, inventory_report_config):
+        """
+        Fetches all inventory report metadata from GCS based on the specified
+        inventory report config.
+
+        Parameters:
+            gcs_file_system (GCSFileSystem): An instance of the 'GCSFileSystem'
+            class (see 'core.py').
+            inventory_report_config (InventoryReportConfig): An instance of
+            the InventoryReportConfig class representing the inventory report
+            configuration.
+
+        Returns:
+            list: A list containing dictionaries representing the metadata of
+            objects from the inventory reports.
+
+        Raises:
+            ValueError: If the fetched inventory reports are empty.
+        """
+        pass
+
+    def _sort_inventory_report_metadata(unsorted_inventory_report_metadata):
+        """
+        Sorts the inventory report metadata based on the 'timeCreated' field
+        in reverse chronological order.
+
+        Parameters:
+            unsorted_inventory_report_metadata (list): A list of dictionaries
+            representing the metadata of objects from the inventory reports.
+
+        Returns:
+            list: A sorted list of dictionaries representing the inventory
+            report metadata, sorted in reverse chronological order based
+            on 'timeCreated'.
+        """
+        pass
+
+    async def _download_inventory_report_content(
+            gcs_file_system, inventory_report_metadata, bucket):
+        """
+        Downloads the most recent inventory report content from GCS based on
+        the inventory report metadata.
+
+        Parameters:
+            gcs_file_system (GCSFileSystem): An instance of the 'GCSFileSystem'
+            class (see 'core.py').
+            inventory_report_metadata (list): A list of dictionaries
+            representing the metadata of objects from the inventory reports.
+            bucket (str): The name of the GCS bucket containing
+            the inventory reports.
+
+        Returns:
+            list: A list containing the content of the most recent inventory
+            report as strings.
+        """
+        pass
+    
+    def _parse_inventory_report_content(gcs_file_system, inventory_report_content,
+            inventory_report_config, use_snapshot_listing, bucket):
+        """
+        Parses the raw inventory report content and extracts object details.
+
+        Parameters:
+            gcs_file_system (GCSFileSystem): An instance of the 'GCSFileSystem'
+            class (see 'core.py').
+            inventory_report_content (list): A list of strings containing the
+            raw content of the inventory report.
+            inventory_report_config (InventoryReportConfig): An instance of the
+            InventoryReportConfig class representing the inventory report
+            configuration.
+            use_snapshot_listing (bool): A flag indicating whether to use snapshot
+            listing in the inventory report.
+            bucket (str): The name of the GCS bucket containing the inventory
+            reports.
+
+        Returns:
+            list: A list of dictionaries representing object details parsed
+            from the inventory report content.
+        """
+        pass
+    
+    def _parse_inventory_report_line(inventory_report_line, use_snapshot_listing, 
+            gcs_file_system, inventory_report_config, delimiter, bucket):
+        """
+        Parses a single line of the inventory report and extracts object details.
+
+        Parameters:
+            inventory_report_line (str): A string representing a single line of
+            the raw content from the inventory report.
+            use_snapshot_listing (bool): A flag indicating whether to use snapshot
+            listing in the inventory report.
+            gcs_file_system (GCSFileSystem): An instance of the 'GCSFileSystem'
+            class (see 'core.py').
+            inventory_report_config (InventoryReportConfig): An instance of the
+            InventoryReportConfig class representing the inventory report
+            configuration.
+            delimiter (str): The delimiter used in the inventory report content
+            to separate fields.
+            bucket (str): The name of the GCS bucket containing the inventory
+            reports.
+
+        Returns:
+            dict: A dictionary representing object details parsed from the
+            inventory report line.
+        """
+        pass
+
+    def _construct_final_snapshot(objects, prefix, use_snapshot_listing):
+        """
+        Constructs the final snapshot based on the retrieved objects and prefix.
+
+        Parameters:
+            objects (list): A list of dictionaries representing object details
+            from the inventory report.
+            prefix (str): A prefix used to filter objects in the snapshot based
+            on their names.
+            use_snapshot_listing (bool): A flag indicating whether to use snapshot
+            listing in the inventory report.
+
+        Returns:
+            tuple: A tuple containing two lists: the 'items' list representing
+            object details for the snapshot, and the 'prefixes' list containing
+            subdirectory prefixes. If 'use_snapshot_listing' is set to False,
+            'prefix' will also be empty, and 'items' will contains the object 
+            names in the snapshot.
+        """
+        pass
+
+    @staticmethod
+    def _convert_obj_to_date(obj):
+        """
+        Converts a dictionary representing a date object to a datetime object.
+
+        Parameters:
+            obj (dict): A dictionary representing a date object with keys "day",
+            "month", and "year".
+
+        Returns:
+            datetime: A datetime object representing the converted date.
+        """
+        pass
+    
+    @staticmethod
+    def _convert_str_to_datetime(str):
+        """
+        Converts an ISO-formatted date string to a datetime object.
+
+        Parameters:
+            date_string (str): An ISO-formatted date string with or without
+            timezone information (Z).
+
+        Returns:
+            datetime: A datetime object representing the converted date and time.
+        """
+        pass
