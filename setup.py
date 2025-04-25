291c8cd7d111dffb5d4f110d47b5f83a435fb6c9
Bjørn Lie Rapp <90246213+Bjoern-Rapp@users.noreply.github.com>
2025-04-25 10:33:45 +0200

    Move config from setup.cfg and setup.py and requirements from requirements.txt to pyproject.toml. Update versioneer.

--- setup.py
+++ setup.py
@@ -1,38 +1,8 @@
 #!/usr/bin/env python
 
-import os
-
 from setuptools import setup
-
 import versioneer
 
-setup(
-    name="gcsfs",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Convenient Filesystem interface over GCS",
-    url="https://github.com/fsspec/gcsfs",
-    maintainer="Martin Durant",
-    maintainer_email="mdurant@anaconda.com",
-    license="BSD",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-    ],
-    keywords=["google-cloud-storage", "gcloud", "file-system"],
-    packages=["gcsfs", "gcsfs.cli"],
-    install_requires=[open("requirements.txt").read().strip().split("\n")],
-    long_description=(
-        open("README.rst").read() if os.path.exists("README.rst") else ""
-    ),
-    extras_require={"gcsfuse": ["fusepy"], "crc": ["crcmod"]},
-    python_requires=">=3.9",
-    zip_safe=False,
-)
+setup(version=versioneer.get_version(),
+     cmdclass=versioneer.get_cmdclass()
+     )
