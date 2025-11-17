c99c9b5476531ee06c1794f9eeef287238b19741
Sunidhi Chandra <sunidhichandra@google.com>
2025-11-17 09:46:01 +0000

    Support write mode in ZonalFile and override related methods

--- gcsfs/zonal_file.py
+++ gcsfs/zonal_file.py
@@ -22,6 +22,10 @@ class ZonalFile(GCSFile):
             self.mrd = asyn.sync(
                 self.gcsfs.loop, self._init_mrd, self.bucket, self.key, self.generation
             )
+        elif "w" in self.mode:
+            self.aaow = asyn.sync(
+                self.gcsfs.loop, self._init_aaow, self.bucket, self.key, self.generation
+            )
         else:
             raise NotImplementedError(
                 "Only read operations are currently supported for Zonal buckets."
@@ -47,10 +51,76 @@ class ZonalFile(GCSFile):
                 return b""
             raise
 
+    async def _init_aaow(
+        self, bucket_name, object_name, generation=None, overwrite=True
+    ):
+        """
+        Initializes the AsyncAppendableObjectWriter.
+        """
+        if generation is None and await self.gcsfs._exists(self.path):
+            info = self.gcsfs.info(self.path)
+            generation = info.get("generation")
+
+        return await zb_hns_utils.init_aaow(
+            self.gcsfs.grpc_client, bucket_name, object_name, generation, overwrite
+        )
+
+    def flush(self, force=False):
+        raise NotImplementedError(
+            "Write operations are not yet implemented for Zonal buckets."
+        )
+
+    def commit(self):
+        raise NotImplementedError(
+            "Write operations are not yet implemented for Zonal buckets."
+        )
+
+    def discard(self):
+        raise NotImplementedError(
+            "Write operations are not yet implemented for Zonal buckets."
+        )
+
+    async def initiate_upload(
+        fs,
+        bucket,
+        key,
+        content_type="application/octet-stream",
+        metadata=None,
+        fixed_key_metadata=None,
+        mode="overwrite",
+        kms_key_name=None,
+    ):
+        raise NotImplementedError(
+            "Write operations are not yet implemented for Zonal buckets."
+        )
+
+    async def simple_upload(
+        fs,
+        bucket,
+        key,
+        datain,
+        metadatain=None,
+        consistency=None,
+        content_type="application/octet-stream",
+        fixed_key_metadata=None,
+        mode="overwrite",
+        kms_key_name=None,
+    ):
+        raise NotImplementedError(
+            "Write operations are not yet implemented for Zonal buckets."
+        )
+
+    async def upload_chunk(fs, location, data, offset, size, content_type):
+        raise NotImplementedError(
+            "Write operations are not yet implemented for Zonal buckets."
+        )
+
     def close(self):
         """
         Closes the ZonalFile and the underlying AsyncMultiRangeDownloader.
         """
         if self.mrd:
             asyn.sync(self.gcsfs.loop, self.mrd.close)
+        if self.aaow:
+            asyn.sync(self.gcsfs.loop, self.aaow.close)
         super().close()
