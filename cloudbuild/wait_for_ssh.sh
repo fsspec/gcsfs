939ed487dbb6f9b5617587f12b10e9c708b89570
suni72 <sunidhichandra@google.com>
2026-02-25 13:30:10 +0000

    implement flag to guarantee cleanup execution on failure Move ssh key cleanup to cleanup phase.

--- cloudbuild/wait_for_ssh.sh
+++ cloudbuild/wait_for_ssh.sh
@@ -3,13 +3,6 @@ set -e
 VM_NAME=$1
 ZONE=$2
 
-echo "--- Removing SSH key from OS Login ---"
-
-gcloud compute os-login describe-profile | \
-grep "fingerprint:" | \
-awk '{print $2}' | \
-xargs -I {} gcloud compute os-login ssh-keys remove --key={} || true
-
 for i in {1..10}; do
   if gcloud compute ssh ${VM_NAME} \
     --zone=${ZONE} \
