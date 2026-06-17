b72d789035bd96080c160144a0fa5d4358c254cb
Li Yonghui <yonghuili@google.com>
2026-06-17 07:31:17 +0000

    re-add comment

--- gcsfs/zb_hns_utils.py
+++ gcsfs/zb_hns_utils.py
@@ -537,10 +537,12 @@ class MRDPool:
                 raise RuntimeError("Cannot initialize a closed MRDPool.")
 
             if not self._initialized and self._active_count == 0:
-                if not self.finalized:
-                    mrd = await self._create_mrd()
-                else:
+                if self.finalized:
                     mrd = await self._get_or_create_mrd()
+                else:
+                    # Always create a new MRD for unfinalized objects to get the up-to-date persisted_size
+                    mrd = await self._create_mrd()
+                    self._all_mrds.append(mrd)
                 self.persisted_size = mrd.persisted_size
                 self._free_mrds.put_nowait(mrd)
                 self._active_count += 1
